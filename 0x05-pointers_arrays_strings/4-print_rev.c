1b3868cc659f7e849a51db76dc2ef5ce4a6d7061
Ryomou <wanjofugrace@gmail.com>
2023-05-03 18:16:06 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_rev- prints a string, in reverse, followed by a new line
+ *
+ *@s:pointer string
+ *
+ * Return void
+ */
+
+void print_rev(char *s);
+{
+	int r = 0;
+
+	while (s[r])
+		r++;
+
+	while (r--)
+	{
+		_putchar(s[r]);
+	}
+	_putchar('\n');
+}
+
